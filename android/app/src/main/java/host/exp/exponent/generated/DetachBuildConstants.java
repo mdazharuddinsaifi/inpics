e4a94749d72fcb4654353436e94c811e2c9796b9
azharsaifi <mdazharuddinsaifi@gmail.com>
2020-02-01 21:05:12 +0530

    gradle build successfull

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expf026bc282b524618a5918f572aad0701://192.168.0.130:19000";
 
 }
